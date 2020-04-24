4f21d8e98b7cd38688fca35b87beb44e3cdd5f9b
mihrac5545samsun <mihracgyahoo.com>
2020-04-24 10:57:28 -0700

    add hellogitworld3.java

--- src/tests1/hellogitworld3.java
+++ src/tests1/hellogitworld3.java
@@ -2,6 +2,6 @@ package tests1;
 
 public class hellogitworld2 {
     public static void main(String[] args) {
-        System.out.println("hello my dear code");
+        System.out.println("hello my dear");
     }
 }
